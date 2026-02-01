57efeae8a04e7e099077099aef1d5dc6036b3de2
Nikita Shevchenko <bladdem2017@gmail.com>
2026-02-01 12:53:23 +0300

    repo contract

---
+++ internal/repo/contracts.go
@@ -0,0 +1,22 @@
+package repo
+
+import (
+	"account-manager/internal/entity"
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type (
+	AccountRepo interface {
+		Create(context.Context, entity.Account) (entity.Account, error)
+		GetByID(ctx context.Context, id uuid.UUID) (entity.Account, error)
+		GetBalanceById(ctx context.Context, id uuid.UUID) (int64, error)
+		GetByIDForUpdate(ctx context.Context, id uuid.UUID) (entity.Account, error)
+	}
+
+	TransactionRepo interface {
+		Create(context.Context, entity.Account) error
+		GetByClientID(ctx context.Context, id uuid.UUID) (entity.Account, error)
+	}
+)
