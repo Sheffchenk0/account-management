05a40791b6feecd49df87ef4374e18272cd3c2ce
Nikita Shevchenko <bladdem2017@gmail.com>
2026-02-01 20:06:35 +0300

    controller layer + app.go

---
+++ internal/app/app.go
@@ -0,0 +1,117 @@
+package app
+
+import (
+	v1 "account-manager/internal/controller/http/v1"
+	"account-manager/internal/infrastructure/rabbitmq"
+	repoPG "account-manager/internal/repo/postgres"
+	"account-manager/internal/service"
+	"account-manager/internal/worker"
+	"account-manager/pkg/postgres"
+	rmqPkg "account-manager/pkg/rabbitmq"
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Config struct {
+	PGURL    string
+	RMQURL   string
+	HTTPPORT string
+}
+
+// TODO: move in cfg
+const (
+	exchangeName           = "account_events"
+	outboxWorkerIntervalMs = 100
+	outboxWorkerBatchSize  = 10
+	readTimeOutS           = 10
+	writeTimeOutS          = 10
+)
+
+func Run(cfg *Config) error {
+	pgPool, err := postgres.New(cfg.PGURL)
+	if err != nil {
+		return fmt.Errorf("postgres init: %w", err)
+	}
+	defer pgPool.Close()
+
+	rmqConn, err := rmqPkg.New(cfg.RMQURL)
+	if err != nil {
+		return fmt.Errorf("rabbitmq init: %w", err)
+	}
+	defer rmqConn.Close()
+
+	txMgr := postgres.NewManager(pgPool.Pool)
+
+	accountRepo := repoPG.NewAccountRepo(txMgr)
+	outboxRepo := repoPG.NewOutboxRepo(txMgr)
+	transactionRepo := repoPG.NewTransactionRepo(txMgr)
+
+	rmqPublisher, err := rabbitmq.NewPublisher(rmqConn, exchangeName)
+	if err != nil {
+		return fmt.Errorf("publisher init %w:", err)
+	}
+
+	transactionService := service.NewTransactionService(
+		txMgr,
+		accountRepo,
+		transactionRepo,
+		outboxRepo,
+	)
+
+	outboxProcessor := worker.NewOutboxProcessor(
+		txMgr,
+		outboxRepo,
+		rmqPublisher,
+		outboxWorkerIntervalMs*time.Millisecond,
+		outboxWorkerBatchSize,
+	)
+
+	handler := gin.Default()
+	handler.Use(gin.Recovery())
+
+	v1Group := handler.Group("/v1")
+	{
+		transactionHandler := v1.NewTransactionHandler(*transactionService)
+		transactionHandler.RegisterRoutes(v1Group)
+	}
+
+	server := &http.Server{
+		Addr:         ":" + cfg.HTTPPORT,
+		Handler:      handler,
+		ReadTimeout:  readTimeOutS * time.Second,
+		WriteTimeout: writeTimeOutS * time.Second,
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		outboxProcessor.Start(ctx)
+	}()
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			// TODO: add logs
+			os.Exit(1)
+		}
+	}()
+
+	<-ctx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		// TODO: add logs
+	}
+
+	return nil
+}
---
+++ internal/controller/http/v1/transaction.go
@@ -0,0 +1,79 @@
+package v1
+
+import (
+	"account-manager/internal/entity"
+	"account-manager/internal/service"
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type createTransactionRequest struct {
+	ID        string `json:"id" binding:"required,uuid"`
+	AccountID string `json:"account_id" binding:"required,uuid"`
+	Amount    int64  `json:"amount" binding:"required gt=0"`
+	Type      string `json:"type" binding:"required,oneof=credit debit"`
+}
+
+type TransactionHandler struct {
+	service service.TransactionService
+}
+
+func NewTransactionHandler(service service.TransactionService) *TransactionHandler {
+	return &TransactionHandler{service}
+}
+
+func (h *TransactionHandler) RegisterRoutes(r *gin.RouterGroup) {
+	r.POST("/transactions", h.Create)
+}
+
+func (h *TransactionHandler) Create(c *gin.Context) {
+	const op = "TransactionHandler.Create"
+
+	var req createTransactionRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	transaction := entity.Transaction{
+		ID:        uuid.MustParse(req.ID),
+		AccountID: uuid.MustParse(req.AccountID),
+		Amount:    req.Amount,
+		Type:      req.Type,
+	}
+
+	err := h.service.CreateTransaction(c.Request.Context(), transaction)
+	if err != nil {
+		h.handleError(c, err, op)
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status": "created",
+		"id":     req.ID,
+	})
+}
+
+func (h *TransactionHandler) handleError(c *gin.Context, err error, op string) {
+	if errors.Is(err, entity.ErrDuplicateTransaction) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "already_processed",
+			"info":   "transaction with this id already exists",
+		})
+		return
+	}
+
+	switch {
+	case errors.Is(err, entity.ErrInsufficientFunds):
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": "insufficient funds"})
+	case errors.Is(err, entity.ErrAccountNotFound):
+		c.JSON(http.StatusNotFound, gin.H{"error": "account not found"})
+	case errors.Is(err, entity.ErrInvalidTransactionType):
+		c.JSON(http.StatusNotFound, gin.H{"error": "invalid transaction type"})
+	default:
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+	}
+	return
+}
